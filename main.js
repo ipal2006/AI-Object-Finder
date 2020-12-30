b53e5836fe5fa2d14c700778d9537d3bec502ab9
ipal2006 <72303965+ipal2006@users.noreply.github.com>
2020-12-30 22:07:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 status="";
 find = "";
-
+objects=[]
 function preload()
 {
    
@@ -13,7 +13,7 @@ function setup()
     video = createCapture(VIDEO);
     video.size(380,380);
     video.hide();
-    
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
 }
 
 function modelLoaded()
@@ -22,12 +22,42 @@ function modelLoaded()
     status=true;
     
 }
-
+function gotResult(error,results)
+{
+if(error)
+{
+    console.error(error);
+}
+console.log(results);
+objects = results;
+}
 function start()
 {
     dectector_of_object = ml5.objectDetector('cocossd',modelLoaded);
     document.getElementById("status").innerHTML = "Detecting Objects";
     find = document.getElementById("object_to_find").value
+    if(status!="")
+    {
+      objectDetector.detect(video,gotResult);
+      for (i=0;i<objects.length;i++)
+      {
+        document.getElementById("status").innerHTML = "Status :- Object Detected";
+        
+
+        fill("orange");
+        percentage = floor(objects[i].confidence*100);
+        text(objects[i].label+" "+percentage+" %",objects[i].x+15,objects[i].y+15);
+        noFill();
+        stroke("orange");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        if(objects[i].label == find)
+      {
+            document.getElementById("dectect").innerHTML = find + " Found";
+      }
+      }
+
+      
+    }
 }
 function draw()
 { 
